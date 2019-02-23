007a141df48483e740e2ed171bbc217651408f29
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-02-23 18:22:46 +0530

    Event Handler Programme
    Event Handler Programme in Java

---
+++ src/eventhanding/EventHanding.java
@@ -0,0 +1,15 @@
+
+package eventhanding;
+
+import javax.swing.JFrame;
+public class EventHanding {
+
+    
+    public static void main(String[] args) {
+        NewClass obj = new NewClass();
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.setSize(350,100);
+        obj.setVisible(true);
+    }
+    
+}
